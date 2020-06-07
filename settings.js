d556095ca3845b658862db84b2080867c34908bb
Yegor Bitensky <yegorbitensky@gmail.com>
2020-06-07 15:09:57 +0300

    Pick color scheme

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
-const BOARD_COLOR = '#cccccc';
-const SNAKE_COLOR = 'black';
-const STOPPED_SNAKE_COLOR = 'orange';
-const SPEED_UP_SNAKE_COLOR = 'red';
-const SPEED_DOWN_SNAKE_COLOR = 'blue';
-const DROP_TAIL_SNAKE_COLOR = 'violet';
-const GROWING_COLOR = 'green';
+const BOARD_COLOR = '#dcd5cd';
+const SNAKE_COLOR = '#322e3d';
+const STOPPED_SNAKE_COLOR = '#ffe17f';
+const SPEED_UP_SNAKE_COLOR = '#ffa376';
+const SPEED_DOWN_SNAKE_COLOR = '#99c2c9';
+const DROP_TAIL_SNAKE_COLOR = '#e4698f';
+const GROWING_COLOR = '#d1d415';
 
 const DROP_TAIL_PERIODICITY = 10;
